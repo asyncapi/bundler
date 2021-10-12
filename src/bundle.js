547e55f2b43ccd6e3b6fa2c17fe8f69c26818584
Souvik <souvikde.ns@gmail.com>
2021-10-12 15:04:23 +0530

    Update bundle.js

--- src/bundle.js
+++ src/bundle.js
@@ -11,6 +11,9 @@ class Bundler {
    * @param {stringp[]} specs 
    */
   async bundle(specs){
+    if(!Array.isArray(specs) && (typeof specs[0] === 'string' || typeof specs[0] === 'object')) {
+      throw new InvalidInputError();
+    }
     const documents = await parsedObjects(specs);
     let doc = {};
     documents.forEach(el => {
