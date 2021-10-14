b54bd8e79dbfbf4eb3326607ee44756f098dba0d
Souvik <souvikde.ns@gmail.com>
2021-10-14 10:31:20 +0530

    feat: added support for yaml output

--- bin/bundle.js
+++ bin/bundle.js
@@ -1,36 +1,51 @@
 #!/usr/bin/env node
-const {Command} = require('commander');
-const bundler = require('../src');
-const fs = require('fs');
-const path = require('path');
+const { Command } = require("commander");
+const bundler = require("../src");
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
 
 const program = new Command();
 
 program
-  .name('asyncapi-bundler')
+  .name("asyncapi-bundler")
   .addHelpCommand(false)
-  .option('-o, --output <path>', 'Specify the output path')
-  .option('-f, --folder', 'All inputs are taken as folder paths')
-  .argument('<filepaths>', 'paths of the file to bundle')
+  .option("-o, --output <path>", "Specify the output path")
+  .option("-f, --folder", "All inputs are taken as folder paths")
+  .argument("<filepaths>", "paths of the file to bundle");
 
 program.parse(process.argv);
 
 const args = program.args;
 const opts = program.opts();
 
-async function main(files, options){
-  if(Array.isArray(files) && typeof files[0] === 'string') {
-    const doc = await bundler.bundle(files.map(file => fs.readFileSync(
-      path.resolve(process.cwd(), file),
-      'utf-8'
-    )));
-    if(options.output) {
-      fs.writeFileSync(path.resolve(process.cwd(), options.output), JSON.stringify(doc));
-      console.log('File created!');
-    }else {
-      console.log(doc)
+async function main(files, options) {
+  if (Array.isArray(files) && typeof files[0] === "string") {
+    const doc = await bundler.bundle(
+      files.map((file) =>
+        fs.readFileSync(path.resolve(process.cwd(), file), "utf-8")
+      )
+    );
+    if (options.output) {
+      const ext = path.extname(options.output);
+      if (ext === ".yml" || ext === ".yaml") {
+        fs.writeFileSync(
+          path.resolve(process.cwd(), options.output),
+          yaml.dump(doc),
+          { encoding: "utf-8" }
+        );
+      }
+      if (ext === ".json") {
+        fs.writeFileSync(
+          path.resolve(process.cwd(), options.output),
+          JSON.stringify(doc)
+        );
+      }
+      console.log("File created!");
+    } else {
+      console.log(doc);
     }
   }
 }
 
-main(args, opts).catch(e => console.error(e));
+main(args, opts).catch((e) => console.error(e));
---
+++ tests/README.md
@@ -0,0 +1,4 @@
+# Example
+
+I would like to create a simple example of the usecase for the bundler and what it is trying to solve. 
+
