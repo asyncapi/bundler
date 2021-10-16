1191021bfc267f079a8abdde1983ef62744d86ff
Souvik <souvikde.ns@gmail.com>
2021-10-16 14:32:51 +0530

    feat: added base option to retain a base spec

--- bin/bundle.js
+++ bin/bundle.js
@@ -12,6 +12,7 @@ program
   .addHelpCommand(false)
   .option("-o, --output <path>", "Specify the output path")
   .option("-f, --folder", "All inputs are taken as folder paths")
+  .option('-b, --base <path>', "File path of the base asyncapi file whose data will be retained")
   .argument("<filepaths>", "paths of the file to bundle");
 
 program.parse(process.argv);
@@ -21,10 +22,14 @@ const opts = program.opts();
 
 async function main(files, options) {
   if (Array.isArray(files) && typeof files[0] === "string") {
+    let base;
+    if(options.base) {
+      base = fs.readFileSync(path.resolve(process.cwd(), options.base), 'utf-8');
+    }
     const doc = await bundler.bundle(
       files.map((file) =>
         fs.readFileSync(path.resolve(process.cwd(), file), "utf-8")
-      )
+      ), {base: base}
     );
     if (options.output) {
       const ext = path.extname(options.output);
--- src/bundle.js
+++ src/bundle.js
@@ -3,14 +3,17 @@ const {
   parsedObjects
 } = require('./utils');
 const _ = require('lodash');
+const yaml = require('js-yaml');
 
 class Bundler {
   
   /**
    * 
    * @param {stringp[]} specs 
+   * @param {Object} option
+   * @param {string} option.base
    */
-  async bundle(specs){
+  async bundle(specs, option){
     if(!Array.isArray(specs) && (typeof specs[0] === 'string' || typeof specs[0] === 'object')) {
       throw new InvalidInputError();
     }
@@ -19,6 +22,9 @@ class Bundler {
     documents.forEach(el => {
       doc = _.merge(doc, el.spec);
     })
+    if(option.base) {
+      doc = _.merge(doc, yaml.load(option.base))
+    }
     return doc;
   }
 }
